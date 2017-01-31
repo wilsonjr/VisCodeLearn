b4a1eaa2ba91a9b624209dee32aca20d57d9c788
wilson_jr <wilson_jr@outlook.com>
2017-01-31 09:17:43 -0200

    Add barchart example with D3

---
+++ bar_chart/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<!-- 
+		attention: this project is not mine and can be found at: https://bost.ocks.org/mike/bar
+		I am just posting these examples for educational purposes
+	-->
+
+	<head>
+		<meta charset='utf-8'>
+		<title>Bar chart - D3.js</title>
+
+
+		<script type="text/javascript" src='http://d3js.org/d3.v3.min.js'></script>
+		<script src="js/jquery.js"></script>
+		<script type="text/javascript" src='js/script.js'></script>
+
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<svg class='chart'></svg>
+
+	</body>
+</html>
