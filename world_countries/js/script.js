6ebf82ca07dfbc065d8b18bd0542166d6dc95057
wilson_jr <wilson_jr@outlook.com>
2017-02-22 20:52:53 -0300

    Add js code to draw world

---
+++ world_countries/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+	function draw(geo_data) {
+
+		'use strict';
+		var margin = 75,
+			width = 1920-margin,
+			height = 1080-margin;
+
+		var svg = d3.select('body')
+			.append('svg')
+			.attr('width', width+margin)
+			.attr('height', height+margin)
+			.append('g')
+			.attr('class', 'map');
+
+		var projection = d3.geo.mercator()
+							.scale(170)
+							.translate([width/2, height/2]);
+		var path = d3.geo.path().projection(projection);
+
+		var map = svg.selectAll('path')
+					.data(geo_data.features)
+					.enter()
+					.append('path')
+					.attr('d', path);
+
+	};
+
+
+
+	d3.json('dataset/world_countries.json', draw);
+});
