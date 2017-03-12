c5d4b3a8868d13875a5289ad3e784e5a9f007081
wilson_jr <wilson_jr@outlook.com>
2017-03-12 16:31:16 -0300

    Finish script to filter unnecessary data

--- scholarship/data/filter_data.py
+++ scholarship/data/filter_data.py
@@ -4,24 +4,23 @@ def filter_file(filename, output, columns):
 
 	if len(columns) != 0:
 		f = open(filename, 'rU')
+		foutput = open(output, 'w')
+		
 		columns_desc = f.readline().split(';')
 		desc_index = []
 		for column in columns:
 			desc_index.append(columns_desc.index(column))
 
-
+		foutput.write(';'.join(columns)+'\n')
 		for line in f:
-			print(line)
+			line_columns = line.split(';')
+			
+			selected_columns = []
+			for index in desc_index:
+				selected_columns.append(line_columns[index])
 
-		print(columns_desc)
-		print(desc_index)
-
-
-
-
-
-
-
+			foutput.write(';'.join(selected_columns)+'\n')
+			
 def main():
 
 	columns = ['ANO_CONCESSAO_BOLSA', 'SEXO_BENEFICIARIO_BOLSA', 'REGIAO_BENEFICIARIO_BOLSA', 'SIGLA_UF_BENEFICIARIO_BOLSA']
