65270cd3039c593f4c1c7e2aeb13480324496fb5
wilson_jr <wilson_jr@outlook.com>
2017-03-12 17:18:20 -0300

    Create visualization area

--- scholarship/js/script.js
+++ scholarship/js/script.js
@@ -3,7 +3,21 @@ $(document).ready(function(){
 
 	function draw(data) {
 
-		
+		'use strict';
+		var margin = 50,
+			width = 960-margin,
+			height = 500-margin;
+
+		d3.select('body')
+			.append('svg')
+			.attr('width', width+margin)
+			.attr('height', height+margin);
+
+		var svg = d3.select('svg')
+			.append('g')
+				.class('class', 'chart');
+
+
 
 	}
 
